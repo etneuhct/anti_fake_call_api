3b31c8cb34d72901ac9f399ac34e87b77543103f
konoufo <konoufo@hotmail.fr>
2024-09-30 06:55:41 -0400

    adds post-deploy command to create default phone number in database

--- docker/runme.sh
+++ docker/runme.sh
@@ -3,5 +3,5 @@ cp nginx_config.conf /etc/nginx/conf.d/django_app.conf
 python manage.py migrate
 python manage.py create_admin
 python manage.py init_demo_user
-#python manage.py add_default_phone_number
+python manage.py add_default_phone_number
 uvicorn api_core.asgi:application --port 8000 & nginx -g "daemon off;"
